4687eb1f40b9633fff24b18e1be2fac654f9d8f8
Forehand, Dayton <daytonforehand@live.com>
2022-01-14 18:35:22 -0600

    added goobla academy image

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 import './projects.css'
 import Thumbnail from './Thumbnail'
 import pokeball from '../../assets/pokeball.svg';
+import goob from '../../assets/goobla_academy1.JPG'
 
 
 
 export default function Projects(props) {
     const projects = [
       {
-        image: pokeball,
-        title: "pokemon-tcg-bot",
-        link: "https://github.com/daytoncf/pokemon-tcg-bot",
-        description: "shiitttt"
+        image: goob,
+        title: "Goobla Academy API",
+        link: "https://github.com/jacksonwelsh/goobla-server",
+        description: "In collaboration with a teammate of mine at HackDFW 2021, we developed a REST API integrated with AWS' NoSQL service DynamoDB to serve data to our Vue app."
       },
       {
         image: pokeball,
@@ -29,6 +30,9 @@ export default function Projects(props) {
 
     return (
         <section id='projects'>
+          <div className='projects__title'>
+            <h2>Here are some things I've built</h2>
+          </div>
           <div className="projects__container">
             {projects.map((project) => <Thumbnail project={project} key={project.image} />)}
           </div>  
