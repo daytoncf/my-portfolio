7d55d7e48178e4603cabf65c2ea2189a2518888a
Forehand, Dayton <daytonforehand@my.unt.edu>
2022-01-20 10:57:37 -0600

    Added smooth scrolling effect, edit about page layout, added some media queries to header and about sections

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,19 +6,20 @@ export default function About(props) {
     return (
         <div className='about'>
             <div className='about__container'>
-                <div>
+                <div className='img__container'>
+                    <img src={me} className="about__img" alt='Me at a flooded park with a hoodie placed awkwardly around my shoulders'/>
+                </div>
+                <div className='text__container'>
                     <h1 className="about__title">
                     About me
                     </h1>
                     <p className="about__desc">
-                        computer science student
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
                     </p>
-                </div>
-                <div className='img__container'>
-                    <img src={me} className="about__img" alt='Me at a flooded park with a hoodie placed awkwardly around my shoulders'/>
-                </div>
-                
-                
+                    <p className="about__desc">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+                    </p>
+                </div>     
             </div>
         </div>
     )
